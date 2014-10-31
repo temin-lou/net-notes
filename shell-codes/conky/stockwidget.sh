77a5ed0985138fbd7580de1274b8233ecbd7168c
Temin Lou <temin.lou@gmail.com>
2014-10-31 15:08:38 +0800

    add some stocks

--- shell-codes/conky/stockwidget.sh
+++ shell-codes/conky/stockwidget.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-URL="http://hq.sinajs.cn/list=s_sh000001,s_sh600795,s_sh601600"
+URL="http://hq.sinajs.cn/list=s_sh000001,s_sh600795,s_sh601600,s_sh600640,s_sz399001,s_sz000001,s_sz000666"
 
 curl -s --connect-timeout 30 $URL | iconv -f gb2312 -t utf-8 |  awk -F\" '{ print $2 }'  | awk 'BEGIN { FS="," } \
-{ if (NF==6) { printf("%1.4s\t %7.2f\t %7.2f %7.2f\n", $1, $2, $3, $4)}}'
+{ if (NF==6) { printf("%1.4s\t %7.2f\t %7.2f %7.2f%%\n", $1, $2, $3, $4)}}'
