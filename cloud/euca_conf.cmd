1fbbe113c1e5176daed7f5bfd6b544d0d220fde0
Temin Lou <temin.lou@gmail.com>
2014-11-07 10:03:58 +0800

    add euca_conf command

---
+++ cloud/euca_conf.cmd
@@ -0,0 +1,3 @@
+euca_conf --register-cluster --partition cluster01 --host 10.10.11.158 --component cc_158
+euca_conf --register-walrusbackend --partition walrus --host 10.10.11.158 --component walrus_158
+euca_conf --register-nodes "10.10.11.158"
