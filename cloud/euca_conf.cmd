f60e63bd94a51d154c80f42dca79f370c9085f17
Temin Lou <temin.lou@gmail.com>
2014-11-12 09:53:48 +0800

    add clean log

--- cloud/euca_conf.cmd
+++ cloud/euca_conf.cmd
@@ -1,3 +1,4 @@
 euca_conf --register-cluster --partition cluster01 --host 10.10.11.158 --component cc_158
 euca_conf --register-walrusbackend --partition walrus --host 10.10.11.158 --component walrus_158
 euca_conf --register-nodes "10.10.11.158"
+> cloud-cluster.log; > cloud-debug.log;> cloud-error.log;> cloud-exhaust.log ; > cloud-output.log ;> cloud-requests.log;> httpd-cc_error_log;> startup.log;
